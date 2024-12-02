82b65698811cc919b8fb5d7fdf6ecdceeca7ecb5
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-12-02 15:05:52 -0700

    sfa "what happens _?"

--- qdd.sh
+++ qdd.sh
@@ -243,6 +243,8 @@ get_statement_from_answer() {
 		sed_command='s/What am \(.*\)\? \(.*\)/\1 am \2/'
 	elif [[ $line =~ "What does it mean to" ]]; then
 		sed_command='s/What does it mean to \(.*\)\? \(.*\)/To \1 means to \2/'
+	elif [[ $line =~ "What happens" ]]; then
+		sed_command='s/What happens \(.*\)\? \(.*\)/\1, \2/'
 	elif [[ $line =~ "Why is" ]] && [[ $line =~ "so" ]]; then
 		sed_command='s/Why is \(.*\) so \(.*\)\? \(.*\)/\1 is so \2 because \3/'
 	elif [[ $line =~ "Why is" ]]; then
