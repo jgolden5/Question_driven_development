777d517f0e08c785eb5f86cf0b57817bb7a942cf
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-01-21 19:37:16 -0700

    sfa "Where is _ located?"

--- qdd.sh
+++ qdd.sh
@@ -613,6 +613,8 @@ get_statement_from_answer() {
     sed_command='s/What \(.*\)s are \(.*\)\? \(.*\)/Some \1s that are \2 include \3/'
   elif [[ $line =~ "Where is " ]] && [[ $line =~ " stored?" ]]; then
     sed_command='s/Where is \(.*\) stored\? \(.*\)/\1 is stored \2/'
+  elif [[ $line =~ "Where is " ]] && [[ $line =~ " located?" ]]; then
+    sed_command='s/Where is \(.*\) located\? \(.*\)/\1 is located \2/'
   elif [[ $line =~ "When should" ]]; then
     sed_option="-r"
     sed_command='s/When should ([^ ]+) (.*)\? (.*)/\1 should \2 when \3/'
