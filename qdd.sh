8dde3b3434d96d29861bd6025b52c512d26d189c
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-03-06 10:49:52 -0700

    sfa "How can a _ be _?"

--- qdd.sh
+++ qdd.sh
@@ -871,6 +871,8 @@ get_statement_from_answer() {
     sed_command='s/How does ([^ ]+) ([^ ]+) (.*)\? (.*)/\1 \2s \3 by \4/'
   elif [[ $line =~ "How do" ]]; then
     sed_command='s/How do \(.*\)\? \(.*\)/\1 by \2/'
+  elif [[ $line =~ "How can " ]] && [[ $line =~ " be " ]]; then
+    sed_command='s/How can \(.*\) be \(.*\)\? \(.*\)/\1 can be \2 by \3/'
   elif [[ $line =~ "How can" ]]; then
     sed_option="-r"
     sed_command='s/How can ([^ ]+) (.*)\? (.*)/\1 can \2 by \3/'
