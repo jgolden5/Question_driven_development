aa3a3b79f042604d41d76eb6c94f0ea4cf21c2f1
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-04-01 09:36:25 -0700

    move get_statements_from_answers from qdd.sh into its own script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ~/bash/bash-debugger
-source ~/bash/Apps/question_driven_development/qdd.sh
-source ~/script-util/jutil
-cd Libraries/prometheus/
-change_term "prometheus"
-debug
-qfr
